12497fb9351ffcec51f503873fa78699b67c219b
Ayur Gonchikov <gonchikov@inbox.ru>
2022-04-14 19:42:18 +0800

    lesson4

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -5,4 +5,25 @@
 @endsection
 @section('content')
   <p>Первое веб приложение реализованное на фреймворке laravel</p>
+  <div class="raw">
+    <p>Обратная связь</p>
+    @if($errors->any())
+      @foreach($errors->all() as $error)
+        <x-alert type="danger" :message="$error"></x-alert>
+      @endforeach
+    @endif
+    <form method="post" action="{{ route('about.store') }}" enctype="multipart/form-data">
+      @csrf
+      <div class="form-group">
+        <label for="firstName">Имя</label>
+        <input type="text" class="form-control" name="firstName" id="firstName" value="{{ old('firstName') }}"/>
+      </div>
+      <div class="form-group">
+        <label for="comment">Комментарий</label>
+        <textarea class="form-control" name="comment" id="comment">{!! old('comment') !!}</textarea>
+      </div>
+      <br>
+      <button type="submit" class="btn btn-success">Сохранить</button>
+    </form>
+  </div>
 @endsection
