cf3d84b616ae4cbd8a1302e87d5938602bb38402
Ayur Gonchikov <gonchikov@inbox.ru>
2022-05-06 13:47:43 +0800

    lesson8

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\NewsController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ReportController;
+
+use App\Http\Controllers\Account\IndexController as AccountController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 use App\Http\Controllers\Admin\IndexController as AdminIndexController;
@@ -41,11 +43,44 @@ Route::get('category/{id}', [CategoryController::class, 'show'])
 Route::resource('/about', AboutController::class);
 Route::resource('/report', ReportController::class);
 
-//Admin routes
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
-    Route::get('/', AdminIndexController::class)
-        ->name('index');
-    Route::resource('categories', AdminCategoryController::class);
-    Route::resource('news', AdminNewsController::class);
-    Route::resource('sources', AdminSourcesController::class);
+//
+Route::group(['middleware' => 'auth'], function() {
+    Route::group(['prefix' => 'account', 'as' => 'account.'], function() {
+        Route::get('/', AccountController::class)->name('index');
+        
+        //logout
+        Route::get('logout', function() {
+            \Auth::logout();
+            return redirect()->route('login');
+        })->name('logout');
+    });
+    //Admin routes
+    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin.check'], function() {
+        Route::get('/', AdminIndexController::class)
+            ->name('index');
+        Route::resource('categories', AdminCategoryController::class);
+        Route::resource('news', AdminNewsController::class);
+        Route::resource('sources', AdminSourcesController::class);
+    });
 });
+
+
+Route::get('example', function() {
+    $array = ['names' => ["Mike", "Pike"], 'name' => ["Ann", "Kate", "Jhon", "Chris", "Ivan"]];
+    $collection = collect($array);
+
+    dd($collection->collapse());
+});
+
+Route::get('session', function() {
+    $name = 'exampleSession';
+    if(session()->has($name)) {
+        // dd(session()->all(), session()->get($name));
+        session()->forget($name);
+    }
+    dd(session()->all());
+    session([$name => 'test']);
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
